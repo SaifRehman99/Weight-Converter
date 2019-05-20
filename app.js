4cd392b17b2c3c77ac432a68adc4e2c48ae8f9e2
SaifRehman99 <saifr7493@gmail.com>
2019-05-20 21:19:24 +0500

    adding the new features

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const resultDiv = document.querySelector('#results');
 const gramsOutput = document.querySelector('#gramsOutput');
 const kgOutput = document.querySelector('#kgOutput');
 const ounceOutput = document.querySelector('#ounceOutput');
-const inputDiv = document.querySelector('#poundData');
+const inputDivP = document.querySelector('#poundData');
+const inputDivG = document.querySelector('#gmData');
+const inputDivK = document.querySelector('#kgData');
+const inputDivO = document.querySelector('#ounceData');
+const selectList = document.querySelector('#selectItem');
+const inputPound = document.querySelector('#pound');
+const inputgram = document.querySelector('#gram');
+const inputkg = document.querySelector('#kilo');
+const inputounce = document.querySelector('#ounce');
+
+// hiding the input fields
+inputPound.style.display = 'none'
+inputgram.style.display = 'none'
+inputkg.style.display = 'none'
+inputounce.style.display = 'none'
+
 
 // setting the card div none
 resultDiv.style.display = 'none'
 
 // adding the event listener
-inputDiv.addEventListener('input', (e) => {
+inputDivP.addEventListener('input', (e) => {
+    console.log(e.target.value)
 
 
     e.target.value ? resultDiv.style.display = 'block' : resultDiv.style.display = 'none'
@@ -19,4 +35,42 @@ inputDiv.addEventListener('input', (e) => {
     gramsOutput.innerHTML = `${poundData / 0.0022046}`;
     kgOutput.innerHTML = `${poundData / 2.2046}`;
     ounceOutput.innerHTML = `${poundData * 16}`;
+})
+inputDivG.addEventListener('input',(e)=>{
+    console.log(e.target.value)
+
+})
+inputDivK.addEventListener('input',(e)=>{
+   
+    console.log(e.target.value)
+    
+})
+inputDivO.addEventListener('input',(e)=>{
+    console.log(e.target.value)
+
+    
+})
+selectList.addEventListener('change', (e) => {
+    if (e.target.value === 'gram') {
+        inputgram.style.display = 'flex'
+
+    }
+    else if (e.target.value === 'kg') {
+        inputkg.style.display = 'flex'
+
+
+    }
+    else if (e.target.value === 'ounce') {
+        inputounce.style.display = 'flex'
+
+
+    }
+    else if (e.target.value === 'pound') {
+        inputPound.style.display = 'flex'
+    }
+    else {
+        inputPound.style.display = 'flex'
+
+    }
+
 })
