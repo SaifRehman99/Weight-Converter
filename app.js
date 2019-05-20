07ba094c3edc380569fffb2f1190b5ef8bd66de6
SaifRehman99 <saifr7493@gmail.com>
2019-05-20 20:48:28 +0500

    Adding the event listener

---
+++ app.js
@@ -0,0 +1,8 @@
+// getting the refrences here
+const resultDiv = document.querySelector('#results');
+const gramsOutput = document.querySelector('#gramsOutput');
+const kgOutput = document.querySelector('#kgOutput');
+const ounceOutput = document.querySelector('#ounceOutput');
+const inputDiv = document.querySelector('#poundData');
+
+// adding the event listener
