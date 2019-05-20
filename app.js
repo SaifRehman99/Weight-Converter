a80a134569c0f9a58b56725b4ed5e5679cdac1bf
SaifRehman99 <saifr7493@gmail.com>
2019-05-20 20:54:27 +0500

    Formula added

--- app.js
+++ app.js
@@ -5,4 +5,18 @@ const kgOutput = document.querySelector('#kgOutput');
 const ounceOutput = document.querySelector('#ounceOutput');
 const inputDiv = document.querySelector('#poundData');
 
+// setting the card div none
+resultDiv.style.display = 'none'
+
 // adding the event listener
+inputDiv.addEventListener('input', (e) => {
+
+
+    e.target.value ? resultDiv.style.display = 'block' : resultDiv.style.display = 'none'
+
+    // getting the input data
+    let poundData = e.target.value;
+    gramsOutput.innerHTML = `${poundData / 0.0022046}`;
+    kgOutput.innerHTML = `${poundData / 2.2046}`;
+    ounceOutput.innerHTML = `${poundData * 16}`;
+})
